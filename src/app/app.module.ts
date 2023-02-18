1ce3729d0504e041ec50e2071deea85e4ad5a051
mtaghizada <magatagi0@gmail.com>
2023-02-18 15:54:31 +0400

    prisma was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { LocalPrismaModule } from '../local-prisma/local-prisma.module';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [LocalPrismaModule],
   controllers: [AppController],
   providers: [],
 })
