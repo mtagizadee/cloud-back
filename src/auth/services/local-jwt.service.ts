5802f8b1774536ed5f490dd999fbd73f80a47fb2
mtaghizada <magatagi0@gmail.com>
2023-02-19 12:25:34 +0400

    generation of access token and refresh token was added

--- src/auth/services/local-jwt.service.ts
+++ src/auth/services/local-jwt.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt/dist';
+import { TJwtPayload } from '../types/jwt-payload.type';
 
 @Injectable()
-export class LocalJwtService {}
+export class LocalJwtService {
+  constructor(private readonly jwt: JwtService) {}
+
+  /**
+   * Generates an access token
+   * @param payload - The payload to generate the access token with
+   * @returns The access token
+   */
+  generateAccessToken(payload: TJwtPayload): string {
+    return this.jwt.sign(payload, {
+      secret: process.env.ACCESS_TOKEN_SECRET,
+      expiresIn: '1d',
+    });
+  }
+
+  /**
+   * Generates a refresh token
+   * @param payload - The payload to generate the refresh token with
+   * @returns The refresh token
+   */
+  generateRefreshToken(payload: TJwtPayload): string {
+    return this.jwt.sign(payload, {
+      secret: process.env.REFRESH_TOKEN_SECRET,
+      expiresIn: '7d',
+    });
+  }
+}
