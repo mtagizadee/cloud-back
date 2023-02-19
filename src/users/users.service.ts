cd4c19c74023f564aa88d0307585aa948dfe162a
mtaghizada <magatagi0@gmail.com>
2023-02-19 12:09:31 +0400

    user findOne method was added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,9 @@
-import { Injectable, ConflictException } from '@nestjs/common';
-import { User } from '@prisma/client';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
 import { PrismaErrors } from '../common/helpers/prisma-errors.helper';
 import { LocalPrismaService } from '../local-prisma/local-prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -7,6 +11,18 @@ import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: LocalPrismaService) {}
+
+  /**
+   * Finds a user by the where clause
+   * @param where - The where clause to find the user with
+   * @returns The user that was found
+   */
+  async findOne(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    const user = await this.prisma.user.findUnique({ where });
+    if (!user) throw new NotFoundException('User is not found.');
+
+    return user;
+  }
 
   /**
    * Creates a new user
